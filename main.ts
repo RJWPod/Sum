9505927698964c77367b9e0f4333ca7d20047f2b
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-04-19 09:54:29 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    let index = 0
+    for (let index = 0; index <= 10; index++) {
+        index += 1
+        index = index + 1
+    }
+    basic.showNumber(index)
+})
+basic.forever(function () {
+	
+})
